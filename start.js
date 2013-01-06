f56a354301f267422cfcf1d67f7322778dc806e0
dan L <danlangford@gmail.com>
2013-01-06 01:04:35 -0500

    moving to express

--- start.js
+++ start.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
-var httpServer = require('http-server');
-
-var port = process.env.PORT || 5000,  // c9&heroku use PORT
+var express = require('express'),
+    app = express.createServer(),
+    
+    port = process.env.PORT || 5000,  // c9&heroku use PORT
     host = process.env.IP || '0.0.0.0'; // c9 uses IP, heroku uses 0.0.0.0
+    
+app.use(express.static(__dirname+'/public'));
 
-(function listen(port) {
-  var server = httpServer.createServer({
-    root:process.env.PWD+'/public' // c9 & heroku both have a PWD
-  });
-
-  server.listen(port, host, function() { });
-})(port);
-
+app.listen(port, host);
