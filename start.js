806ffc0f8369224e8e5dd8db1dea80f4f10eda30
dan L <danlangford@gmail.com>
2013-01-06 00:54:31 -0500

    heroku fixes

--- start.js
+++ start.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 var httpServer = require('http-server');
 
-var port = process.env.PORT || 5000,
-    host = process.env.IP || '127.0.0.1';
-
-console.log(process.env);
+var port = process.env.PORT || 5000,  // c9&heroku use PORT
+    host = process.env.IP || '0.0.0.0'; // c9 uses IP, heroku uses 0.0.0.0
 
 (function listen(port) {
   var server = httpServer.createServer({
-      
-    root:process.env.PWD
+    root:process.env.PWD+'/public' // c9 & heroku both have a PWD
   });
 
   server.listen(port, host, function() { });
