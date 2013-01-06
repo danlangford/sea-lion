a91634214a9ad82a57f87b90ce608072f2bd74b3
dan L <danlangford@gmail.com>
2013-01-06 00:49:15 -0500

    fixed Procfile

--- start.js
+++ start.js
@@ -4,9 +4,12 @@ var httpServer = require('http-server');
 var port = process.env.PORT || 5000,
     host = process.env.IP || '127.0.0.1';
 
+console.log(process.env);
+
 (function listen(port) {
   var server = httpServer.createServer({
-    root:'.'
+      
+    root:process.env.PWD
   });
 
   server.listen(port, host, function() { });
