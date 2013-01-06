af7600572d82f0da55a42bbadfebe9ca73624673
dan L <danlangford@gmail.com>
2013-01-06 14:17:25 -0500

    following angular-seed patterns in prep for angular-ifying this page

--- start.js
+++ start.js
@@ -5,6 +5,6 @@ var express = require('express'),
     port = process.env.PORT || 5000,  // c9&heroku use PORT
     host = process.env.IP || '0.0.0.0'; // c9 uses IP, heroku uses 0.0.0.0
     
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/app'));
 
 app.listen(port, host);
